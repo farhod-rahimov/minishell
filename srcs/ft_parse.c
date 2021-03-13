04463aed23e0832952fa2bde17827c79e3432153
Farhod Rahimov <farhod.rahimov0055@gmail.com>
2021-03-13 10:56:03 +0300

    разделенная строка закидывается в структуру. Без ; работает, лики проверь. Скорее всего они есть

--- srcs/ft_parse.c
+++ srcs/ft_parse.c
@@ -6,7 +6,7 @@
 /*   By: btammara <btammara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 16:52:30 by btammara          #+#    #+#             */
-/*   Updated: 2021/03/13 09:30:04 by btammara         ###   ########.fr       */
+/*   Updated: 2021/03/13 10:54:57 by btammara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,35 +22,43 @@ int		ft_parse(t_struct *strct, t_args *tmp, int i)
 	if ((str = (char *)malloc(sizeof(char) * k + 1)) == NULL)
 		return (-1);
 	
+	while (strct->parsed_str[i] == ' ')
+		i++;
 	while (strct->parsed_str[i])
 	{
-		while (strct->parsed_str[i] == ' ')
-			i++;
 		if (strct->parsed_str[i] == ';')
 		{
 			if (k == 1)
 				return (-1); // "  ; parsed_str" or "  parsed str    ;  ;"
+			
+			if (i > 1)
+				if (strct->parsed_str[i - 1] != ' ')
+					if ((ft_copy_str_to_structure_args(tmp, str, strct->n, strct->n_i)) == -1)	
+						return (-1); //malloc error
+			
 			if ((tmp = ft_create_new_t_args(tmp)) == NULL)
-				return (-1);
-			if (ft_parse(strct, tmp, ++i) == -1)
 				return (-1);
 			strct->n++;
 			strct->n_i = 0;
+			
+			return (ft_parse(strct, tmp, ++i));
 		}
-		else
+		if (strct->parsed_str[i] == ' ')
 		{
-			str[k - 1] = strct->parsed_str[i];	
-			str[k++] = '\0';
-			tmp_str = str;
-
-			if ((str = (char *)malloc(sizeof(char) * (k + 1))) == NULL)
+			if ((ft_copy_str_to_structure_args(tmp, str, strct->n, strct->n_i)) == -1)	
 				return (-1); //malloc error
-			ft_copy_old_str(tmp_str, str);
-			free(tmp_str);
-			if ((ft_copy_str_to_structure_args(tmp, str, strct->n, strct->n_i++)) == -1)	
-				return (-1); //malloc error
-			exit(-21);
+			strct->n_i++;
+			return (ft_parse(strct, tmp, i));
 		}
+
+		str[k - 1] = strct->parsed_str[i];	
+		str[k++] = '\0';
+		tmp_str = str;
+		if ((str = (char *)malloc(sizeof(char) * (k + 1))) == NULL)
+			return (-1); //malloc error
+		ft_copy_old_str(tmp_str, str);
+		free(tmp_str);
+
 		i++;
 	}
 	return (0);
@@ -58,50 +66,44 @@ int		ft_parse(t_struct *strct, t_args *tmp, int i)
 
 int		ft_copy_str_to_structure_args(t_args *tmp, char *str, int n, int n_i)
 {
-	// char ***tmp_arg; // tmp->arg
+	char **tmp_arg; // tmp->arg
+	(void)n;
 	
 	if (n_i == 0)
 	{
-		printf("n = %d, n_i = %d\n", n, n_i);
-		printf("%s\n", str);
-		exit(-21);
-		if ((tmp->arg = (char ***)malloc(sizeof(char **) * (n_i + 2))) == NULL)
+		if ((tmp->arg = (char **)malloc(sizeof(char *) * (n_i + 2))) == NULL)
 			return (-1);
-		if ((tmp->arg[n][n_i] = ft_strdup(str)) == NULL) // n_i = 0
+		if ((tmp->arg[n_i] = ft_strdup(str)) == NULL) // n_i = 0
 			return (-1);
-		tmp->arg[1] = NULL;
+		tmp->arg[n_i + 1] = NULL;
 		return (0);
 	}
 	
-	// tmp_arg = tmp->arg;
-	// if ((tmp->arg = (char ***)malloc(sizeof(char **) * (n_i + 2))) == NULL) // n_i for old strs, 1 for new str, 1 for NULL
-	// 	return (-1);
-	// ft_copy_old_arg_n_i_of_tmp(tmp, tmp_arg, n, n_i);
-	// if ((tmp->arg[n][n_i] = ft_strdup(str)) == NULL)
-	// 	return (-1);
-	// tmp->arg[n_i + 1] = NULL;
-	// free(tmp_arg);
+	tmp_arg = tmp->arg;
+	if ((tmp->arg = (char **)malloc(sizeof(char *) * (n_i + 2))) == NULL) // n_i for old strs, 1 for new str, 1 for NULL
+		return (-1);
+	if ((ft_copy_old_arg_n_i_of_tmp(tmp, tmp_arg, n, n_i)) == -1)
+		return (-1);
+	if ((tmp->arg[n_i] = ft_strdup(str)) == NULL)
+		return (-1);
+	tmp->arg[n_i + 1] = NULL;
+	free(tmp_arg);  // не до конца правильно очищаю нужно кажду строку в этом 2-ух мерном массиве
 	return (0);
 }
 
-void	ft_copy_old_arg_n_i_of_tmp(t_args *tmp, char ***tmp_arg, int n, int n_i)
+int		ft_copy_old_arg_n_i_of_tmp(t_args *tmp, char **tmp_arg, int n, int n_i)
 {
-	int i;
-	int k;
+	int tmp_n_i;
+	(void)n;
 
-	i = 0;
-	k = 0;
-	while (i < n_i)
+	tmp_n_i = 0;
+	while (tmp_n_i < n_i)
 	{
-		while (tmp_arg[n][i][k])
-		{
-			tmp->arg[n][i][k] = tmp_arg[n][i][k];
-			k++;
-		}
-		tmp->arg[n][i][k] = '\0';
-		k = 0;
-		i++;
+		if ((tmp->arg[tmp_n_i] = ft_strdup(tmp_arg[tmp_n_i])) == NULL)
+			return (-1);
+		tmp_n_i++;
 	}
+	return (0);
 }
 
 t_args	*ft_create_new_t_args(t_args *prev_t_args)
@@ -110,9 +112,31 @@ t_args	*ft_create_new_t_args(t_args *prev_t_args)
 
 	if ((new_t_args = (t_args *)malloc(sizeof(t_args))) == NULL)
 		return (NULL);
+	new_t_args->next = NULL;
 		
 	if (prev_t_args != NULL)
 		prev_t_args->next = new_t_args;
 	
 	return (new_t_args);
+}
+
+void	ft_print_devided_args(t_args *head)
+{
+	t_args	*tmp;
+	int		i;
+
+	tmp = head;
+	i = 0;
+	printf("--------------------\n");
+	while (tmp)
+	{
+		while (tmp->arg[i])
+		{
+			printf("%s\n", tmp->arg[i]);
+			i++;
+		}
+		printf("------------------------\n");
+		i = 0;
+		tmp = tmp->next;
+	}
 }
