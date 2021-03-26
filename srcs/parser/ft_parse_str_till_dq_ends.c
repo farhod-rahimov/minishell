71789488f9eda09dba4b36fa340985d3ef6c2ba9
Farhod Rahimov <farhod.rahimov0055@gmail.com>
2021-03-26 17:06:06 +0300

    ft_parse_str_till_dq_ends NORMED

--- srcs/parser/ft_parse_str_till_dq_ends.c
+++ srcs/parser/ft_parse_str_till_dq_ends.c
@@ -6,35 +6,21 @@
 /*   By: btammara <btammara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 13:56:32 by btammara          #+#    #+#             */
-/*   Updated: 2021/03/26 15:37:39 by btammara         ###   ########.fr       */
+/*   Updated: 2021/03/26 17:04:20 by btammara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static	int		ft_remove_back_slash(char **str, int i)
-{
-	char	*tmp;
-	int		initial_i;
-
-	tmp = (*str);
-	initial_i = i;
-	while ((*str)[i])
-	{
-		(*str)[i] = tmp[i + 1];
-		i++;
-	}
-	return (++initial_i);
-}
-
-static char		*ft_get_env_var_value(char *env_var, t_struct *strct)
+static	char	*ft_get_env_var_value(char *env_var, t_struct *strct)
 {
 	t_env	*tmp;
 
 	tmp = strct->env_head;
 	while (tmp)
 	{
-		if (!ft_strncmp(env_var, tmp->key, ft_strlen(env_var) + ft_strlen(tmp->key)))
+		if (!ft_strncmp(env_var, tmp->key, \
+			ft_strlen(env_var) + ft_strlen(tmp->key)))
 		{
 			env_var = ft_strdup_new(tmp->value);
 			return (env_var);
@@ -45,20 +31,18 @@ static char		*ft_get_env_var_value(char *env_var, t_struct *strct)
 	return (env_var);
 }
 
-static	void	ft_replace_env_key_to_its_value(char **str, char *env_value, int start_env_var, int len_env_var)
+static	void	ft_replace_env_key_to_its_value(char **str, char *env_value, \
+							int start_env_var, int len_env_var)
 {
 	char *tmp1;
 	char *tmp2;
-	
+
 	tmp1 = *str;
-	// printf("str1 %s, start_env_var %d, len_env_var %d\n", *str, start_env_var, len_env_var);
 	if ((*str = ft_substr(*str, 0, start_env_var)) == NULL)
 		ft_write_malloc_error();
-	
 	tmp2 = *str;
 	*str = ft_strjoin_new(*str, env_value);
 	free(tmp2);
-	
 	tmp2 = *str;
 	*str = ft_strjoin_new(*str, tmp1 + start_env_var + len_env_var);
 	free(tmp2);
@@ -74,32 +58,29 @@ static	int		ft_get_env_var(char **str, int i, t_struct *strct)
 	start_env_var = i - 1;
 	env_var = ft_strdup_new("");
 	while ((*str)[i] && (ft_isalnum((*str)[i]) || (*str)[i] == '_'))
-	{
 		ft_push_back_char(&env_var, (*str)[i++]);
-	}
-
-	// printf("env_key %s\n", env_var);
 	env_key = env_var;
 	env_var = ft_get_env_var_value(env_var, strct);
-	// printf("env_value %s\n", env_var);
 	if (env_var[0] != '\0')
-		ft_replace_env_key_to_its_value(str, env_var, start_env_var, ft_strlen(env_key) + 1);
-	// printf("str get_env_var %s\n", (*str));
+		ft_replace_env_key_to_its_value(str, env_var, \
+			start_env_var, ft_strlen(env_key) + 1);
 	free(env_key);
-	free(env_var);	
+	free(env_var);
 	return (i);
 }
 
-static void		ft_work_with_spec_sym(char **str, int i, t_struct *strct)
+static	void	ft_work_with_spec_sym(char **str, int i, t_struct *strct)
 {
 	while ((*str)[i])
 	{
-		if ((*str)[i] == '\\' && ((*str)[i + 1] == '\\' || (*str)[i + 1] == '$' || (*str)[i + 1] == '\"'))
+		if ((*str)[i] == '\\' && ((*str)[i + 1] == '\\' \
+			|| (*str)[i + 1] == '$' || (*str)[i + 1] == '\"'))
 		{
 			i = ft_remove_back_slash(str, i);
 			continue ;
 		}
-		else if ((*str)[i] == '$' && (ft_isalnum((*str)[i + 1]) || (*str)[i + 1] == '_'))
+		else if ((*str)[i] == '$' && \
+			(ft_isalnum((*str)[i + 1]) || (*str)[i + 1] == '_'))
 		{
 			i = ft_get_env_var(str, ++i, strct);
 			continue ;
@@ -108,22 +89,23 @@ static void		ft_work_with_spec_sym(char **str, int i, t_struct *strct)
 	}
 }
 
-int		ft_parse_str_till_dq_ends(t_args **current_t_arg, int i, t_struct *strct)
+int				ft_parse_str_till_dq_ends(t_args **current_t_arg, \
+								int i, t_struct *strct)
 {
 	char	*str;
-	
+
 	str = ft_strdup_new("");
 	while (strct->parsed_str[i])
 	{
 		if (i > 0)
-			if (strct->parsed_str[i] == '\"' && strct->parsed_str[i - 1] != '\\')
-				break ; // i stopped at "
+			if (strct->parsed_str[i] == '\"' \
+			&& strct->parsed_str[i - 1] != '\\')
+				break ;
 		ft_push_back_char(&str, strct->parsed_str[i++]);
 	}
-	
 	ft_work_with_spec_sym(&str, 0, strct);
-	ft_copy_str_to_structure_t_args(strct, current_t_arg, str, strct->n_i);	// n_i++
+	ft_copy_str_to_structure_t_args(strct, current_t_arg, str, strct->n_i);
 	free(str);
-	i = ft_check_if_new_list_or_arg_is_needed(strct, current_t_arg, ++i); // ++i for skipping the last 
+	i = ft_check_if_new_list_or_arg_is_needed(strct, current_t_arg, ++i);
 	return (i);
 }
